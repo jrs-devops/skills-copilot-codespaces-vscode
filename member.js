af6a03c65bc47934b0d94195ecc56c44b591ebe4
jrs-devops <156038926+jrs-devops@users.noreply.github.com>
2024-05-24 00:59:54 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,60 @@
+function skillsMember() {
+  var skills = document.getElementById("skills");
+  var member = document.getElementById("member");
+  var memberName = document.getElementById("memberName");
+  var memberSkills = document.getElementById("memberSkills");
+  var memberImg = document.getElementById("memberImg");
+  var memberNameText = document.getElementById("memberNameText");
+  var memberSkillsText = document.getElementById("memberSkillsText");
+  var memberImgSrc = document.getElementById("memberImgSrc");
+  // Member 1
+  var member1 = document.getElementById("member1");
+  member1.onclick = function () {
+    memberNameText.innerHTML = "Milton B. Jr.";
+    memberSkillsText.innerHTML = "Web Developer";
+    memberImgSrc.src = "images/members/member1.jpg";
+  };
+  // Member 2
+  var member2 = document.getElementById("member2");
+  member2.onclick = function () {
+    memberNameText.innerHTML = "Milton B. Jr.";
+    memberSkillsText.innerHTML = "Web Developer";
+    memberImgSrc.src = "images/members/member2.jpg";
+  };
+  // Member 3
+  var member3 = document.getElementById("member3");
+  member3.onclick = function () {
+    memberNameText.innerHTML = "Milton B. Jr.";
+    memberSkillsText.innerHTML = "Web Developer";
+    memberImgSrc.src = "images/members/member3.jpg";
+  };
+  // Member 4
+  var member4 = document.getElementById("member4");
+  member4.onclick = function () {
+    memberNameText.innerHTML = "Milton B. Jr.";
+    memberSkillsText.innerHTML = "Web Developer";
+    memberImgSrc.src = "images/members/member4.jpg";
+  };
+  // Member 5
+  var member5 = document.getElementById("member5");
+  member5.onclick = function () {
+    memberNameText.innerHTML = "Milton B. Jr.";
+    memberSkillsText.innerHTML = "Web Developer";
+    memberImgSrc.src = "images/members/member5.jpg";
+  };
+  // Member 6
+  var member6 = document.getElementById("member6");
+  member6.onclick = function () {
+    memberNameText.innerHTML = "Milton B. Jr.";
+    memberSkillsText.innerHTML = "Web Developer";
+    memberImgSrc.src = "images/members/member6.jpg";
+  };
+  // Member 7
+  var member7 = document.getElementById("member7");
+  member7.onclick = function ()
+    {
+        memberNameText.innerHTML = "Milton B. Jr.";
+        memberSkillsText.innerHTML = "Web Developer";
+        memberImgSrc.src = "images/members/member7.jpg";
+        };
+    } // Add this closing curly brace
